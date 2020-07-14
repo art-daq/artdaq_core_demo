916ee91407928cc42791c65460c30b222be9a787
Eric Flumerfelt <eflumerf@fnal.gov>
2020-07-14 15:26:24 -0500

    Add ArtdaqDemo_fragmentNameHelper to replace ArtdaqDemoFragmentNamingService.

---
+++ artdaq-core-demo/Overlays/ArtdaqDemo_fragmentNameHelper.cc
@@ -0,0 +1,41 @@
+#include "artdaq-core-demo/Overlays/FragmentType.hh"
+#include "artdaq-core/Data/FragmentNameHelper.hh"
+
+#include "TRACE/tracemf.h"
+#define TRACE_NAME "ArtdaqDemoFragmentNamingService"
+
+namespace artdaq {
+/**
+ * \brief ArtdaqDemoFragmentNamingService extends ArtdaqFragmentNamingService.
+ * This implementation uses artdaq-demo's SystemTypeMap and directly assigns names based on it
+ */
+class ArtdaqDemoFragmentNameHelper : public ArtdaqFragmentNameHelper
+{
+public:
+	/**
+	 * \brief DefaultArtdaqFragmentNamingService Destructor
+	 */
+	~ArtdaqDemoFragmentNameHelper() override = default;
+
+	/**
+	 * \brief ArtdaqDemoFragmentNamingService Constructor
+	 */
+	ArtdaqDemoFragmentNameHelper(std::string unidentified_instance_name, std::vector<std::pair<artdaq::Fragment::type_t, std::string>> extraTypes);
+
+private:
+	ArtdaqDemoFragmentNameHelper(ArtdaqDemoFragmentNameHelper const&) = delete;
+	ArtdaqDemoFragmentNameHelper(ArtdaqDemoFragmentNameHelper&&) = delete;
+	ArtdaqDemoFragmentNameHelper& operator=(ArtdaqDemoFragmentNameHelper const&) = delete;
+	ArtdaqDemoFragmentNameHelper& operator=(ArtdaqDemoFragmentNameHelper&&) = delete;
+};
+
+ArtdaqDemoFragmentNameHelper::ArtdaqDemoFragmentNameHelper(std::string unidentified_instance_name, std::vector<std::pair<artdaq::Fragment::type_t, std::string>> extraTypes)
+    : ArtdaqFragmentNameHelper(unidentified_instance_name, extraTypes)
+{
+	TLOG(TLVL_DEBUG) << "ArtdaqDemoFragmentNamingService CONSTRUCTOR START";
+	SetBasicTypes(demo::makeFragmentTypeMap());
+	TLOG(TLVL_DEBUG) << "ArtdaqDemoFragmentNamingService CONSTRUCTOR END";
+}
+}  // namespace artdaq
+
+DEFINE_ARTDAQ_FRAGMENT_NAME_HELPER(artdaq::ArtdaqDemoFragmentNameHelper)
